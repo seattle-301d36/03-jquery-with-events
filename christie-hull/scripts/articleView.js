f4703f500694bc77c2e1da1ee1cdc49e07e7da8c
Hoffit <dehull_1@yahoo.com>
2018-07-12 12:57:24 -0700

    Author and Category filtering is working now.

--- christie-hull/scripts/articleView.js
+++ christie-hull/scripts/articleView.js
@@ -3,7 +3,7 @@
 // REVIEW: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
 let articleView = {};
 
-articleView.jAndDavid = function() {
+articleView.populateFilters = function() {
   $('article').each(function() {
     // REVIEW: We can declare several variables at once and assign their values later when using let. Keep in mind that we cannot do this with const.
     let authorName, category, optionTag;
@@ -13,9 +13,8 @@ articleView.jAndDavid = function() {
       // Start by grabbing the author's name from `this` article element, and then use that bit of text to create the option tag (in a variable named `optionTag`) that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
 
-      // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="${authorName}">${authorName}</option>';
-      // optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+      // TODone: Refactor this concatenation using a template literal.
+      optionTag = `<option value="${authorName}">${authorName}</option>`;
 
       if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
         $('#author-filter').append(optionTag);
@@ -25,9 +24,8 @@ articleView.jAndDavid = function() {
       // Avoid duplicates! We don't want to append the category name if the <select> already has this category as an option!
       category = $(this).attr('data-category');
 
-      // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="${category}">${category}</option>';
-      // optionTag = '<option value="' + category + '">' + category + '</option>';
+      // TODone: Refactor this concatenation using a template literal.
+      optionTag = `<option value="${category}">${category}</option>`;
 
       if ($('#category-filter option[value="' + category + '"]').length === 0) {
         $('#category-filter').append(optionTag);
@@ -40,23 +38,35 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // TODone: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
+      const selectedValue = $(this).val();
+      $('article').hide();
+      $(`article[data-author="${selectedValue}"]`).fadeIn();
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-
+      // TODone: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      $('article[class!="template"]').fadeIn();
     }
     $('#category-filter').val('');
   });
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  $('#category-filter').on('change', function() {
+  // TODone: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
-
+    if ($(this).val()) {
+        const selectedValue = $(this).val();
+        $('article').hide();
+        $(`article[data-category="${selectedValue}"]`).fadeIn();
+    } else {
+        $('article[class!="template"]').fadeIn();
+    }
+    $('#author-filter').val('');
+});
 };
 
 articleView.handleMainNav = function() {
@@ -78,5 +88,7 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
 })
